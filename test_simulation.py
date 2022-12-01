93a1d147252877203f33865efa9f246bf9771b1a
Chris Wood <wchristopherwood912@gmail.com>
2022-11-30 21:43:31 -0800

    cleaned up code

--- test_simulation.py
+++ test_simulation.py
@@ -2,7 +2,6 @@ import pytest
 from simulation import Simulation
 
 def test_create_population():
-    sim = Simulation("oogabooga", 50, 0)
 
     assert len(sim.population) == sim.pop_size
     
@@ -10,6 +9,7 @@ def test_create_population():
         assert sim.population[i].infected == True
 
 def test_interaction():
-    sim = Simulation("oogabooga", 1000, 0)
 
     len(sim.interaction(100))
+
+sim = Simulation("oogabooga", 1000, 0)
