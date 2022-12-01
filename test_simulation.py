f9902a689f6c3c29e79fa050b27647a36407784d
Chris Wood <wchristopherwood912@gmail.com>
2022-11-30 21:54:34 -0800

    added test for infect_newly_infected() method

--- test_simulation.py
+++ test_simulation.py
@@ -12,4 +12,13 @@ def test_interaction():
 
     len(sim.interaction(100))
 
-sim = Simulation("oogabooga", 1000, 0)
+def test_infect_newly_infected():
+
+    sim1 = Simulation("oogabooga", 10000, 0, initial_infected=0) #used 0 initial infections because the check for current infection is in interaction() method 
+
+    sim1._infect_newly_infected(sim1.population)
+
+    assert len(sim1.infected) == 10000
+
+
+sim = Simulation("oogabooga", 10000, 0)
